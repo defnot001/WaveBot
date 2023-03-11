db254ea8b4d92dfaabd348c6928527d5de593f47
defnot001 <marc.miller994@gmail.com>
2023-03-11 20:56:27 +0100

    changed welcomeMessage

--- src/assets/welcomeMessage.ts
+++ src/assets/welcomeMessage.ts
@@ -13,19 +13,19 @@ export const getTrialWelcomeMessage = (client: Client) => {
     },
     {
       name: `${owoKiwi}  Mods & Resources`,
-      value: `On WaveTech we share our waypoints with a mod that Earthcomputer wrote. You can find it in <#${config.channels.resources}>. Also make sure to put the mod for instamineable deepslate in your mods-folder.`,
+      value: `On WaveTech we use a variety of optimization and quality of life mods. You can find it them in <#${config.channels.resources}>.`,
     },
     {
       name: `${owoKiwi}  Server Info`,
-      value: `You can find the IPs of our servers in <#${config.channels.serverInfo}>. There is also instructions on how to connect to KiwiTech using KCP. This is especially useful if you live outside of Europe and/or have unstable connection. Make sure to also read the SMP rules.`,
+      value: `You can find the IPs of our servers in <#${config.channels.serverInfo}>. Make sure to also read the SMP rules.`,
     },
     {
-      name: `${owoKiwi}  Todo on KiwiTech`,
-      value: `When you got your trial member role, we also gave you the Kiwi Inc. role. This role gets pinged from time to time to inform active SMP players about new projects or important things to do on our servers. You can check out our <#${config.channels.todo}> to see what needs to be done or bring your own ideas and discuss them with us.`,
+      name: `${owoKiwi}  Todo on WaveTech`,
+      value: `When you got your provisional member role. This role gets pinged from time to time to inform active SMP players about new projects or important things to do on our servers. You can check out our <#${config.channels.todo}> to see what needs to be done or bring your own ideas and discuss them with us.`,
     },
     {
       name: '\u200b',
-      value: `*The most important thing on KiwiTech is to have fun! If you have any questions, you can always ask us anything in member channels or ingame. We are also active in VC!*  ${froghypers}`,
+      value: `*The most important thing on WaveTech is to have fun! If you have any questions, you can always ask us anything in member channels or ingame. We are also active in VC!*  ${froghypers}`,
     },
   ];
 
